bd442acf2207d1f0667e7b6c07ce62c0c4f24fb7
Neil <neilspurgeon@gmail.com>
2017-11-04 15:14:16 -0700

    animate reveal text

--- src/components/ProjectHeader/ProjectHeader.js
+++ src/components/ProjectHeader/ProjectHeader.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import Grid from 'components/Grid/Grid.js';
 import styles from './style.css';
+import Waypoint from 'react-waypoint';
 
-const ProjectHeader = (props) => {
-  return (
-    <Grid>
-      <div className={styles.wrapper}>
-        <h1 className={styles.title}>{props.title}</h1>
-        <p className={styles.subTitle}>{props.subTitle}</p>
-      </div>
-    </Grid>
-  );
-};
+class ProjectHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      revealClasses: [styles.textRevealText]
+    };
+  }
+
+  handleEnter = () => {
+    this.setState({
+      revealClasses: [styles.textRevealText, styles.inView]
+    });
+  }
+
+  render() {
+    return (
+      <Grid>
+        <div className={styles.wrapper}>
+
+          <Waypoint onEnter={this.handleEnter}>
+            <h1 className={[styles.title, styles.textRevealWrapper].join(' ')}>
+              <span className={this.state.revealClasses.join(' ')}>
+                {this.props.title}
+              </span>
+            </h1>
+          </Waypoint>
+
+          <p className={[styles.subTitle, styles.textRevealWrapper].join(' ')}>
+            <span className={this.state.revealClasses.join(' ')}>
+              {this.props.subTitle}
+            </span>
+          </p>
+
+        </div>
+      </Grid>
+    );
+  }
+}
 
 export default ProjectHeader;
