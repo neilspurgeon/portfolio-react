c35bfc39abb1494b0df83ad4c586288a198b0313
Neil <neilspurgeon@gmail.com>
2017-11-04 14:22:22 -0700

    added project information

--- src/projects/CoffeeBeanWeb/data.js
+++ src/projects/CoffeeBeanWeb/data.js
@@ -1,10 +1,13 @@
+import heroImage from './images/coffeebean-web-hero.jpg';
 import cardImage from './images/coffeebean-web-hero.jpg';
 
 const data = {
-  title: "Coffee Bean E-Commerce",
-  path: "/coffee-bean-web",
-  component: "CoffeBeanWeb",
-  card: cardImage
+  title: 'Coffee Bean E-Commerce',
+  subTitle: '— Website Design, UX Design',
+  hero: heroImage,
+  card: cardImage,
+  path: '/coffee-bean-web',
+  component: 'CoffeBeanWeb'
 };
 
 export default data;
