1317fbb1008b7c8494a459bdf6b93c50ee0718bf
Neil <neilspurgeon@gmail.com>
2017-11-04 16:21:50 -0700

    Use RevealImage

--- src/components/ProjectHero/ProjectHero.js
+++ src/components/ProjectHero/ProjectHero.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Grid from 'components/Grid/Grid.js';
+import RevealImage from 'components/RevealImage/RevealImage.js';
 import styles from './style.css';
 
 const ProjectHero = (props) => {
   return (
     <Grid>
-      <div className={styles.wrapper}>
+      <RevealImage className={styles.wrapper}>
         <img className={styles.image} src={props.image} alt={props.altText} />
-      </div>
+      </RevealImage>
     </Grid>
   );
 };
