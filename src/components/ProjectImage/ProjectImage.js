ef27f2b2cfb94a3f83a3ca6c47812e959a1a0917
Neil <neilspurgeon@gmail.com>
2017-11-04 02:29:52 -0700

    Transitions into view

--- src/components/ProjectImage/ProjectImage.js
+++ src/components/ProjectImage/ProjectImage.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import Grid from 'components/Grid/Grid.js';
 import styles from './style.css';
+import Waypoint from 'react-waypoint';
+import ClassNames from 'classnames';
 
-const ProjectImage = (props) => {
-  return (
-    <Grid>
-      <div className={styles.wrapper}>
-        <img className={styles.image} src={props.image} alt={props.altText} />
-      </div>
-    </Grid>
-  );
+class ProjectImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inView: false
+    };
+  }
+
+  handleEnter = (e) => {
+    this.setState({
+      inView: true
+    });
+  };
+
+  render() {
+    return (
+        <Grid>
+          <Waypoint bottomOffset="200px" onEnter={this.handleEnter}>
+            <div className={ClassNames(styles.imageWrapper, {[styles.inView] : this.state.inView === true})}>
+              <img className={styles.image} src={this.props.image} alt={this.props.altText} />
+            </div>
+          </Waypoint>
+        </Grid>
+
+    );
+  }
+
 };
 
 export default ProjectImage;
