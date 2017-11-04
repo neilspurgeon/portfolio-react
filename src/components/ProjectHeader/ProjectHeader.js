c780a62f47c0e4c236fa8121c6e27c527f112330
Neil <neilspurgeon@gmail.com>
2017-11-03 20:19:11 -0700

    add project header component

---
+++ src/components/ProjectHeader/ProjectHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Grid from 'components/Grid/Grid.js';
+import styles from './style.css';
+
+const ProjectHeader = (props) => {
+  return (
+    <Grid>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>{props.title}</h1>
+        <p className={styles.byLine}>{props.byLine}</p>
+      </div>
+    </Grid>
+  );
+};
+
+export default ProjectHeader;
