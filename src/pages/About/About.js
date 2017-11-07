fe7654c434da645643d999e65bb0f002b1b32460
Neil <neilspurgeon@gmail.com>
2017-11-06 21:03:53 -0800

    Link styling

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,7 +3,7 @@ import Grid from 'components/Grid/Grid.js';
 import Nav from 'components/Nav/Nav.js';
 import Footer from 'components/Footer/Footer.js';
 import styles from './styles.css';
-import index from 'sharedStyles/index.css';
+import sharedStyles from 'sharedStyles/index.css';
 import portrait from './portrait.jpg';
 
 const About = () => {
@@ -18,12 +18,12 @@ const About = () => {
         <main className={styles.wrapper}>
 
           <section className={styles.textSection}>
-            <h1 className={[index.p, styles.sectionLabel].join(' ')}>About</h1>
+            <h1 className={[sharedStyles.p, styles.sectionLabel].join(' ')}>About</h1>
             <p>I’m a designer and developer based in San Luis Obispo, CA. I have a wide range of experience but specialize in digital product design and front end development.</p>
             <p>I’m currently available for freelance work and interesting projects.</p>
-            <p>Please get in touch at <a href="mailto:hello@neilspurgeon.com">hello@neilspurgeon.com</a></p>
+            <p>Please get in touch at <a className={sharedStyles.inlineLink}href="mailto:hello@neilspurgeon.com">hello@neilspurgeon.com</a></p>
 
-            <h2 className={[index.p, styles.sectionLabel].join(' ')}>Experience</h2>
+            <h2 className={[sharedStyles.p, styles.sectionLabel].join(' ')}>Experience</h2>
             <p>Hathway — San Luis Obispo, CA<br />
               <span>Senior Designer, Jan. 2017 - Now</span><br />
               <span>Visual Designer, Sep. 2015 - Dec. 2016</span>
@@ -32,7 +32,7 @@ const About = () => {
               <span>Designer, April. 2014 - April. 2015</span>
             </p>
 
-            <h2 className={[index.p, styles.sectionLabel].join(' ')}>Education</h2>
+            <h2 className={[sharedStyles.p, styles.sectionLabel].join(' ')}>Education</h2>
             <p>California Polytechnic State University — San Luis Obispo, CA<br />
               <span>BFA Art & Design; Graphic Design Concentration</span>
             </p>
@@ -40,7 +40,7 @@ const About = () => {
               <span>Web Development Immersive</span>
             </p>
 
-            <h2 className={[index.p, styles.sectionLabel].join(' ')}>Colophon</h2>
+            <h2 className={[sharedStyles.p, styles.sectionLabel].join(' ')}>Colophon</h2>
             <p>Lorem Ipsum</p>
           </section>
 
