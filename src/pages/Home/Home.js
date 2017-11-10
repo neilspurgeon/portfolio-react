3e3660de2b3594e922ca7bdab22609c357d470ec
Neil <neilspurgeon@gmail.com>
2017-11-09 19:20:21 -0800

    Created staggered grid home screen layout.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,35 +7,117 @@ import styles from './styles.css';
 import sharedStyles from 'sharedStyles/index.css';
 import projects from 'projects';
 
-const Home = () => {
-  return (
-    <div>
-      <Nav />
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
 
-      <Grid>
-        <header className={styles.intro}>
-          <h1 className={styles.introText}>I’m Neil, a digital product  designer & frontend developer.</h1>
-          <Link className={sharedStyles.arrowLink} to='/about'>Learn More</Link>
-        </header>
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
 
-        <section className={styles.projects}>
-          {projects.map((project) => {
-            return (
-              <Link className={styles.card} to={project.data.path}>
-                <div className={styles.imageWrapper}>
-                  <img className={styles.image} src={project.data.card} alt={project.data.title} />
-                </div>
-                <h2 className={styles.title}>{project.data.title}</h2>
-                <p className={styles.subTitle}>{project.data.subTitle}</p>
-              </Link>
-            );
-          })}
-        </section>
-      </Grid>
+  // make sure to remove the listener
+  // when the component is not mounted anymore
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
 
-      <Footer />
-    </div>
-  );
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+
+  render() {
+    let width = this.state.width;
+
+    if (width >= 1200) {
+      return (
+        <div>
+          <Nav />
+          <Grid>
+
+            <header className={styles.intro}>
+              <h1 className={styles.introText}>I’m Neil, a digital product  designer & frontend developer.</h1>
+              <Link className={sharedStyles.arrowLink} to='/about'>Learn More</Link>
+            </header>
+
+            <section className={styles.projects}>
+              <div className={styles.leftCol}>
+                {projects.map((project, i) => {
+                  // dont render every other item
+                  if (i % 2 !== 0) {
+                    return (
+                      <Link className={styles.card} to={project.data.path}>
+                        <div className={styles.imageWrapper}>
+                          <img className={styles.image} src={project.data.card} alt={project.data.title} />
+                        </div>
+                        <h2 className={styles.title}>{project.data.title}</h2>
+                        <p className={styles.subTitle}>{project.data.subTitle}</p>
+                      </Link>
+                    );
+                  }
+                })}
+              </div>
+
+              <div className={styles.rightCol}>
+                {projects.map((project, i) => {
+                  // dont render every other item
+                  if (i % 2 === 0) {
+                    return (
+                      <Link className={styles.card} to={project.data.path}>
+                        <div className={styles.imageWrapper}>
+                          <img className={styles.image} src={project.data.card} alt={project.data.title} />
+                        </div>
+                        <h2 className={styles.title}>{project.data.title}</h2>
+                        <p className={styles.subTitle}>{project.data.subTitle}</p>
+                      </Link>
+                    );
+                  }
+                })}
+              </div>
+
+            </section>
+
+          </Grid>
+          <Footer />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <Nav />
+        <Grid>
+
+          <header className={styles.intro}>
+            <h1 className={styles.introText}>I’m Neil, a digital product  designer & frontend developer.</h1>
+            <Link className={sharedStyles.arrowLink} to='/about'>Learn More</Link>
+          </header>
+
+          <section className={styles.projects}>
+            {projects.map((project, i) => {
+              return (
+                <Link className={styles.card} to={project.data.path}>
+                  <div className={styles.imageWrapper}>
+                    <img className={styles.image} src={project.data.card} alt={project.data.title} />
+                  </div>
+                  <h2 className={styles.title}>{project.data.title}</h2>
+                  <p className={styles.subTitle}>{project.data.subTitle}</p>
+                </Link>
+              );
+            })}
+          </section>
+
+        </Grid>
+        <Footer />
+      </div>
+    );
+
+  };
+
 };
 
 export default Home;
