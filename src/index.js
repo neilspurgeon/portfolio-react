bf8d6d36b7713895ddc89d9bd2fe4b9e93722b16
Neil <neilspurgeon@gmail.com>
2017-11-12 14:09:37 -0800

    Added react-snapshot to create static pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { render } from 'react-snapshot';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import 'sharedStyles/index.css';
 
-ReactDOM.render((
+render(
   <BrowserRouter>
     <Routes />
-  </BrowserRouter>
-), document.getElementById('root'));
+  </BrowserRouter>,
+  document.getElementById('root'));
