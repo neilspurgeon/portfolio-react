1917616a30a40b517df2d5ea7a73281d8a5800bf
Neil <neilspurgeon@gmail.com>
2017-11-14 20:26:59 -0800

    Created basic intro slide.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,55 +8,85 @@ import sharedStyles from 'sharedStyles/index.css';
 import projects from 'projects';
 import ProjectCard from 'components/ProjectCard/ProjectCard.js';
 
-const Home = () => {
-  return (
-    <div>
-      <Nav />
-      <Grid>
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false
+    };
+  }
 
-        <header className={styles.intro}>
-          <h1 className={styles.introText}>I’m Neil, a digital product  designer & frontend developer.</h1>
-          <Link className={sharedStyles.arrowLink} to='/about'>Learn More</Link>
-        </header>
+  componentDidMount = () => {
+    document.body.classList.toggle(styles.loaderBody);
 
-        <section className={styles.projects}>
-          <div className={styles.leftCol}>
-            {projects.map((project, i) => {
-              if (i % 2 !== 0) {
-                return (
-                  <ProjectCard
-                    path={project.data.path}
-                    image={project.data.card}
-                    altText={project.data.title}
-                    title={project.data.title}
-                    subTitle={project.data.subTitle}
-                  />
-                );
-              }
-            })}
+    window.setTimeout(() => {
+      document.body.classList.toggle(styles.loaderBody);
+      this.setState({
+        isLoaded: true
+        });
+    }, 1500);
+  }
+
+  render() {
+
+    const header = (<header className={styles.intro}>
+      <h1 className={styles.introText}>I’m Neil, a digital product  designer & frontend developer.</h1>
+      <Link className={[sharedStyles.arrowLink, styles.introLink].join(' ')} to='/about'>Learn More</Link>
+    </header>);
+
+    return (
+      <div>
+
+        <Nav />
+        <Grid>
+
+          {header}
+
+          <div className={this.state.isLoaded ? [styles.loader, styles.isLoaded].join(' ') : styles.loader}>
+            <Grid>
+              {header}
+            </Grid>
           </div>
 
-          <div className={styles.rightCol}>
-            {projects.map((project, i) => {
-              if (i % 2 === 0) {
-                return (
-                  <ProjectCard
-                    path={project.data.path}
-                    image={project.data.card}
-                    altText={project.data.title}
-                    title={project.data.title}
-                    subTitle={project.data.subTitle}
-                  />
-                );
-              }
-            })}
-          </div>
+          <section className={styles.projects}>
+            <div className={styles.leftCol}>
+              {projects.map((project, i) => {
+                if (i % 2 !== 0) {
+                  return (
+                    <ProjectCard
+                      path={project.data.path}
+                      image={project.data.card}
+                      altText={project.data.title}
+                      title={project.data.title}
+                      subTitle={project.data.subTitle}
+                    />
+                  );
+                }
+              })}
+            </div>
 
-        </section>
-      </Grid>
-      <Footer />
-    </div>
-  );
+            <div className={styles.rightCol}>
+              {projects.map((project, i) => {
+                if (i % 2 === 0) {
+                  return (
+                    <ProjectCard
+                      path={project.data.path}
+                      image={project.data.card}
+                      altText={project.data.title}
+                      title={project.data.title}
+                      subTitle={project.data.subTitle}
+                    />
+                  );
+                }
+              })}
+            </div>
+
+          </section>
+        </Grid>
+        <Footer />
+      </div>
+    );
+  }
 };
 
 export default Home;
