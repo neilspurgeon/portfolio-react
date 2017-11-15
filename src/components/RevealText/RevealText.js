fe513b1a7c3a178afd5e32de199f88efdb23b86c
Neil <neilspurgeon@gmail.com>
2017-11-14 23:31:59 -0800

    Transitions in text

---
+++ src/components/RevealText/RevealText.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './style.css';
+import Waypoint from 'react-waypoint';
+
+class RevealImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      revealClasses: [styles.textRevealText]
+    };
+  }
+
+  handleEnter = () => {
+    this.setState({
+      revealClasses: [styles.textRevealText, styles.inView]
+    });
+  };
+
+  render() {
+
+    const CustomTag = `${this.props.tag}`;
+
+    return (
+      <Waypoint bottomOffset={this.props.bottomOffset || '200px'} onEnter={this.handleEnter}>
+
+          <CustomTag className={this.props.className}>
+
+          {this.props.text.map((text, i) => {
+            let elStyle = {transitionDelay: (.15 * i) + 's'};
+            let classes = this.state.revealClasses;
+            return (
+              <div className={styles.textRevealWrapper}>
+                <span className={classes.join(' ')} style={elStyle}>
+                  {text}
+                </span>
+              </div>
+            );
+          })}
+          </CustomTag>
+
+      </Waypoint>
+    );
+  }
+}
+
+export default RevealImage;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,8 +7,10 @@ import styles from './styles.css';
 import sharedStyles from 'sharedStyles/index.css';
 import projects from 'projects';
 import ProjectCard from 'components/ProjectCard/ProjectCard.js';
+import RevealText from 'components/RevealText/RevealText.js';
 
-const introDuration = 1500;
+const introDuration = 2000;
+const breakPoint = 560;
 
 class Home extends React.Component {
   constructor(props) {
@@ -20,6 +22,9 @@ class Home extends React.Component {
   }
 
   componentWillMount() {
+
+    this.setState({ windowSize: window.innerWidth });
+
     if (window.sessionStorage.getItem('hasViewedIntro')) {
       this.setState({
         showLoader: false
@@ -43,6 +48,24 @@ class Home extends React.Component {
 
       window.sessionStorage.setItem('hasViewedIntro', true);
     }
+
+      window.addEventListener('resize', this.resize);
+  }
+
+  resize = (e) => {
+
+    const lastSize = this.state.windowSize;
+    const newSize = e.target.innerWidth;
+
+    if (lastSize > breakPoint && newSize < breakPoint ) {
+      this.forceUpdate();
+      this.setState({ windowSize: newSize });
+    }
+
+    if (lastSize < breakPoint && newSize > breakPoint ) {
+      this.forceUpdate();
+      this.setState({ windowSize: newSize });
+    }
   }
 
   removeLoader = () => {
@@ -51,13 +74,42 @@ class Home extends React.Component {
     }, introDuration);
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  }
+
   render() {
 
-    const header = (<header className={styles.intro}>
-      <h1 className={styles.introText}>I’m Neil, a digital product  designer & frontend developer.</h1>
-      <Link className={[sharedStyles.arrowLink, styles.introLink].join(' ')} to='/about'>Learn More</Link>
+    let header = (
+      <header className={styles.intro}>
+        <RevealText
+          className={styles.introText}
+          tag={'h1'}
+          text={[
+            'I’m Neil, a digital ',
+            'product designer & ',
+            'frontend developer.'
+          ]}
+        />
+        <Link className={[sharedStyles.arrowLink, styles.introLink].join(' ')} to='/about'>Learn More</Link>
+      </header>
+    );
 
-    </header>);
+    if (window.innerWidth > breakPoint) {
+      header = (
+        <header className={styles.intro}>
+          <RevealText
+            className={styles.introText}
+            tag={'h1'}
+            text={[
+              'I’m Neil, a digital product ',
+              'designer & frontend developer.'
+            ]}
+          />
+          <Link className={[sharedStyles.arrowLink, styles.introLink].join(' ')} to='/about'>Learn More</Link>
+        </header>
+      );
+    }
 
     return (
       <div>
