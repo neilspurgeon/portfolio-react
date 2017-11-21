a15b61e460071a6ab867b6c472108d6c12ff1707
Neil <neilspurgeon@gmail.com>
2017-11-20 19:59:49 -0800

    updated overview

--- src/projects/Fontifi/Fontifi.js
+++ src/projects/Fontifi/Fontifi.js
@@ -4,6 +4,7 @@ import collectionImg from './images/collection.jpg';
 import collectionEmptyImg from './images/collection-empty.jpg';
 import signUpImg from './images/sign-up.jpg';
 import ProjectText from 'components/ProjectText/ProjectText.js';
+import ProjectOverview from 'components/ProjectOverview/ProjectOverview.js';
 import ProjectImage from 'components/ProjectImage/ProjectImage.js';
 import sharedStyles from 'sharedStyles/index.css';
 
@@ -15,14 +16,17 @@ const Fontifi = () => {
   return (
     <div>
 
-      <ProjectText>
+      <ProjectOverview
+        linkUrl="https://www.fontifi.co"
+        linkText="fontifi.co"
+        >
         <p>This is an ongoing side project of mine that started as way learn React and to stay up to date on coding skills that weren’t being used at my full-time position. My goal was to create an interactive site that could be used by designers and developers to help find and explore different web font combinations.</p>
 
         <p className={sharedStyles.pSmall}><span className={sharedStyles.label}>Technology</span><br />
         React<br />
         Express<br />
         MongoDB + mLab</p>
-      </ProjectText>
+      </ProjectOverview>
 
       <ProjectImage
         backgroundStyle={bgStyle}
@@ -34,6 +38,10 @@ const Fontifi = () => {
           }
         ]}
       />
+
+    <ProjectText>
+      <p>I’m currently in the first stage of this project where I’ve been designing and developing a sort-of web font playground that will be used as the foundation going forward. To get things started I created a database that stores about 40 curated Google fonts and will then be added to as the project evolves. Right now, the user can then generate random font combinations from the database and then save them to their own collection for later reference. </p>
+    </ProjectText>
 
       <ProjectImage
         backgroundStyle={bgStyle}
@@ -48,6 +56,10 @@ const Fontifi = () => {
           }
         ]}
       />
+
+    <ProjectText>
+      <p>The next stage will be to create a database of curated combinations instead of randomly selected ones. I want users to be able to submit what they come up with, have it go through an approval process, and then get added to the database. It could then naturally grow from there.</p>
+    </ProjectText>
 
       <ProjectImage
         backgroundStyle={bgStyle}
