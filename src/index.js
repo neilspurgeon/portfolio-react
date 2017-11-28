3429a28626fc7dc93d63847ce1c44e2f1cd43bd4
Neil <neilspurgeon@gmail.com>
2017-11-27 22:14:25 -0800

    removed snapshot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render } from 'react-snapshot';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import 'sharedStyles/index.css';
 
-render(
+ReactDOM.render(
   <BrowserRouter>
     <Routes />
   </BrowserRouter>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
