f48b55426632019844a15ff7f7f284e8a4031f2e
Neil <neilspurgeon@gmail.com>
2017-11-28 19:28:07 -0800

    cleanup

--- src/components/ProjectHero/ProjectHero.js
+++ src/components/ProjectHero/ProjectHero.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Grid from 'components/Grid/Grid.js';
 import Image from 'components/Image/Image.js';
-import transitions from 'sharedStyles/transitions.css';
 import styles from './style.css';
 
 const ProjectHero = (props) => {
