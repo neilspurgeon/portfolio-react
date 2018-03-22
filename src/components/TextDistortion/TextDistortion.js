d2cdaa53a214831c9729a8bbd1fe9dcb654d6520
Neil <neilspurgeon@gmail.com>
2018-03-22 11:23:09 -0700

    animate text with pixi js

--- src/components/TextDistortion/TextDistortion.js
+++ src/components/TextDistortion/TextDistortion.js
@@ -17,12 +17,15 @@ const textStyle = new PIXI.TextStyle({
   wordWrapWidth: width
 });
 
+
+// Create Text
 const text = new PIXI.Text('Neil is a California based designer who likes to code. He is currently looking for full-time career opportunities',
   textStyle);
 text.anchor.x = 0.5;
 text.anchor.y = 0.5;
 text.x = width / 2;
 text.y = height / 2;
+
 
 class TextDistortion extends React.Component {
 
@@ -37,7 +40,42 @@ class TextDistortion extends React.Component {
     });
     this.refs.canvas.appendChild(this.app.view);
 
+    // Draw text
     this.app.stage.addChild(text);
+
+    // Set variables
+    let sprite;
+
+    // Load Textures
+    PIXI.loader
+      .add('https://res.cloudinary.com/dvxikybyi/image/upload/v1486634113/2yYayZk_vqsyzx.png')
+      .load( () => setup() );
+
+    const setup = () => {
+
+      // This code will run when the loader has finished loading the image
+      sprite = new PIXI.Sprite(
+        PIXI.loader.resources['https://res.cloudinary.com/dvxikybyi/image/upload/v1486634113/2yYayZk_vqsyzx.png'].texture
+      );
+
+      sprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+
+      // Create displament filter from sprite
+      let displacementFilter = new PIXI.filters.DisplacementFilter(sprite);
+
+      this.app.stage.addChild(sprite);
+      this.app.stage.filters = [displacementFilter];
+
+      // Create animation
+      this.app.ticker.add(delta => gameLoop(delta));
+    };
+
+    function gameLoop(delta) {
+      sprite.x += .2;
+      sprite.y += .2;
+    }
+
+
   }
 
   render() {
