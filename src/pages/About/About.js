52adcbe79ca4060c1d4b17ffdc5f41f08a7d77a8
Neil <neilspurgeon@gmail.com>
2018-05-02 21:51:40 -0700

    changed about section layout to 2 col

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,12 +4,12 @@ import Header from 'components/Header/Header.js';
 import Footer from 'components/Footer/Footer.js';
 import styles from './styles.css';
 import shared from 'sharedStyles/index.css';
-import portrait from './portrait.jpg';
+import portrait from './profile.jpg';
 import Image from 'components/Image/Image.js';
 import Playlist from 'components/Playlist/Playlist.js';
 import { Helmet } from 'react-helmet';
 
-const headingClass = [shared.label, styles.aboutHeader].join(' ');
+const headingClass = [shared.h3, styles.aboutSectionHeader].join(' ');
 
 const About = () => {
   return (
@@ -21,48 +21,76 @@ const About = () => {
 
       <Header />
 
-      <Container>
-        <div className={styles.pageWrapper}>
+      <Container type="inset">
+        <header className={styles.header}>
           <Image
-            className={styles.portrait}
+            className={styles.headerImage}
             image={portrait}
             altText="portrait of me"
             sizes="100vw, 50vw"
           />
 
-          <div className={styles.text}>
+          <div className={styles.headerText}>
             <p className={shared.h2}>I'm a designer currently based in San Luis Obispo, California. For the past few years, I've focused mainly on web and mobile app design, while also taking great interest in front-end web development.</p>
-
             <p className={shared.h2}>I'm currently looking for a full-time position that focuses on design, but ideally allows opportunities to code as well.</p>
-
-            <div className={styles.body}>
-              <h2 className={headingClass}>Experience</h2>
-              <p>Hathway — San Luis Obispo, CA<br />
-                <span>Senior Designer, Jan. 2017 - Feb. 2018</span><br />
-                <span>Visual Designer, Sep. 2015 - Dec. 2016</span>
-              </p>
-              <p>Indicate Design Groupe — San Francisco, CA<br />
-                <span>Designer, April. 2014 - April. 2015</span>
-              </p>
-
-              <h2 className={headingClass}>Education</h2>
-              <p>California Polytechnic State University — San Luis Obispo, CA<br />
-                <span>BFA Art & Design; Graphic Design Concentration</span>
-              </p>
-              <p>General Assembly — San Francisco, CA <br />
-                <span>Web Development Immersive</span>
-              </p>
-
-              <h2 className={headingClass}>Interests</h2>
-              <p>Outside of design, I love music and you can often find me at shows. I also really like spending time outdoors, whether that be surfing, camping, hiking, or anything else adventurous.<br /></p>
-
-              <h2 className={headingClass}>Currently Listening To</h2>
-              <Playlist className={styles.playlist}/>
-            </div>
-
           </div>
 
-        </div>
+        </header>
+
+
+        <section className={styles.aboutSection}>
+          <h2 className={headingClass}>Experience</h2>
+
+          <div className={styles.aboutSectionContent}>
+            <p>Hathway — San Luis Obispo, CA<br />
+              <span>Senior Designer, Jan. 2017 - Feb. 2018</span><br />
+              <span>Visual Designer, Sep. 2015 - Dec. 2016</span>
+            </p>
+            <p>Indicate Design Groupe — San Francisco, CA<br />
+              <span>Designer, April. 2014 - April. 2015</span>
+            </p>
+          </div>
+        </section>
+
+
+        <section className={styles.aboutSection}>
+          <h2 className={headingClass}>Education</h2>
+
+          <div className={styles.aboutSectionContent}>
+            <p>California Polytechnic State University — San Luis Obispo, CA<br />
+              <span>BFA Art & Design; Graphic Design Concentration</span>
+            </p>
+
+            <p>General Assembly — San Francisco, CA <br />
+              <span>Web Development Immersive</span>
+            </p>
+          </div>
+        </section>
+
+
+        <section className={styles.aboutSection}>
+          <h2 className={headingClass}>Interests</h2>
+
+          <div className={styles.aboutSectionContent}>
+            <p>Outside of design, I love music and you can often find me at shows. I also really like spending time outdoors, whether that be surfing, camping, hiking, or anything else adventurous.</p>
+          </div>
+        </section>
+
+
+        <section className={styles.aboutSection}>
+          <h2 className={headingClass}>Listening To</h2>
+
+          <div className={styles.aboutSectionContent}>
+
+          {/* wrap playlist in P tags to give same spacing */}
+          <p>
+            <Playlist className={styles.playlist} />
+          </p>
+
+          </div>
+        </section>
+
+
       </Container>
 
       <Footer />
